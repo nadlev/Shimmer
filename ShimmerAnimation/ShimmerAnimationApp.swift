81e4262cdc1791ad81569637bf33e54f9900af19
nadlev <nadiya.levytska.jr@gmail.com>
2023-05-02 21:24:53 +0300

    Initial Commit

---
+++ ShimmerAnimation/ShimmerAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerAnimationApp.swift
+//  ShimmerAnimation
+//
+//  Created by Надежда Левицкая on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
